59d479eeeb14b347d1fb1a69ae2fa30d9ae74f40
Igor Bezerra <igor.bezerra96@gmail.com>
2017-11-18 13:24:12 -0300

    Updating function compareWeapons on test src

--- tests/src/RockPaperScissors.js
+++ tests/src/RockPaperScissors.js
@@ -1,33 +1,30 @@
-function compareWeapons(player1Choice, player2Choice) {
+//Weapons list
+const weapons = {
+    rock: {
+        beats: ['scissors'],
+        counter: 0,
+    },
+    paper: {
+        beats: ['rock'],
+        counter: 0,
+    },
+    scissors: {
+        beats: ['paper'],
+        counter: 0,
+    },
+};
 
+/*
+ * Compare weapons to check the winner
+ */
+function compareWeapons(player1Choice, player2Choice) {
+    //Check if it's a tie
     if (player1Choice == player2Choice) {
         return 0;
     }
-
-    if (player1Choice == "rock") {
-    	if (player2Choice == "scissors") { 
-            return 1;         
-        }
-        else {
-            return 2;
-        }
+    //Check if player1's weapon beats player2's weapon
+    if (weapons[player1Choice].beats.indexOf(player2Choice) >= 0) {
+        return 1;         
     }
-    
-    if (player1Choice == "paper") { 	
-        if (player2Choice == "rock") {
-            return 1;
-        }
-        else {
-            return 2;
-        }
-    }
-
-    if (player1Choice == "scissors") {    
-        if (player2Choice == "rock") {
-            return 2;
-        }
-        else {
-            return 1;
-        }
-    }
+    return 2;
 }
