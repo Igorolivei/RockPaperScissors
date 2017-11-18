b97fd841bde584d8d625a894f5f6652c4c8e89ad
Igor Bezerra <igor.bezerra96@gmail.com>
2017-11-17 21:29:05 -0300

    Adding logic to try to predict the play of a player

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+//Play counters
+var playerRocks = 0;
+var playerPapers = 0;
+var playerScissors = 0;
+
 //Scores
-var player2Score = 0
 var player1Score = 0
+var player2Score = 0
 
 //Scores elements
 var player1ScoreBox = document.getElementById('player1Score');
@@ -10,33 +15,66 @@ var player2ScoreBox = document.getElementById('player2Score');
 var player1ChoiceElement = document.getElementById('player1Choice');
 var player2ChoiceElement = document.getElementById('player2Choice')
 
+//Weapons list
+const weapons = {
+    rock: {
+        beats: ['scissors'],
+    },
+    paper: {
+        beats: ['rock'],
+    },
+    scissors: {
+        beats: ['paper'],
+    },
+};
+
+/*
+ * Execute a play in both modes
+ */
 function play(playerChoice) {
 
-	/* Generates computer choice. */
-    player1Choice = playerChoice ? playerChoice : Math.floor(Math.random() * 3) + 1;
-	player2Choice = Math.floor(Math.random() * 3) + 1;
-	
-	if (player2Choice == 1) {
-	    player2Choice = "rock";
-	} else if(player2Choice == 2) {
-	    player2Choice = "paper";
-	} else {
-		player2Choice = "scissors";
-	}
+	// Check if it's player vs computer, and if not, select a random weapon
+    player1Choice = playerChoice ? playerChoice : Math.floor(Math.random() * Object.keys(weapons).length) + 1;
 
-    if (Number.isInteger(player1Choice)) {
-        if (player1Choice == 1) {
-            player1Choice = "rock";
-        } else if(player1Choice == 2) {
-            player1Choice = "paper";
+    //If player vs computer, counts the play
+    if (playerChoice) {
+        if (playerChoice == 'rock') {
+            playerRocks++;
+        } else if (playerChoice == 'paper') {
+            playerPapers++;
         } else {
-            player1Choice = "scissors";
+            playerScissors++;
+        }
+    //If computer vs computer
+    } else {
+        if (player1Choice == 1) {
+            player1Choice = 'rock';
+        } else if (player1Choice == 2) {
+            player1Choice = 'paper';
+        } else {
+            player1Choice = 'scissors';
         }
     }
 
+    //Check if the player has the tendence to play one specific weapon and generates computer's choice
+    var playerTendence = checkPlayerTendencies();
+    player2Choice = playerTendence != 0 ? choosePlay(playerTendence) : Math.floor(Math.random() * Object.keys(weapons).length) + 1;
+	
+    if (Number.isInteger(player2Choice)) {
+        if (player2Choice == 1) {
+            player2Choice = 'rock';
+        } else if (player2Choice == 2) {
+            player2Choice = 'paper';
+        } else {
+            player2Choice = 'scissors';
+        }
+    }
+
+    //Writes on the UI the chosen weapon
     player1ChoiceElement.innerHTML = player1Choice;
     player2ChoiceElement.innerHTML = player2Choice;
 
+    //Gets the winner 
     var winner = compareWeapons(player1Choice, player2Choice);
     if (winner == 1) {
         player1Score++;
@@ -48,52 +86,79 @@ function play(playerChoice) {
     return updateScores();
 }
 
-function compareWeapons(player1Choice, player2Choice) {
+/*
+ * Check if player has tendencies to play the same weapon, and which weapon he plays more
+ * Obs.: Considered just if more than 60% of plays are the same weapon
+ */
+function checkPlayerTendencies() {
+    var totalPlays = playerRocks + playerPapers + playerScissors;
+    var pRock = playerRocks / totalPlays;
+    var pPaper = playerPapers / totalPlays;
+    var pScissors = playerScissors / totalPlays;
 
-    if (player1Choice == player2Choice) {
-        return 0;
-    }
-
-    if (player1Choice == "rock") {
-    	if (player2Choice == "scissors") { 
-            return 1;         
-        }
-        else {
-            return 2;
-        }
-    }
-    
-    if (player1Choice == "paper") { 	
-        if (player2Choice == "rock") {
-            return 1;
-        }
-        else {
-            return 2;
+    //If it's not the first play
+    if (totalPlays > 1) {
+        if (pRock >= 0.60) {
+            return 'rock';
+        } else if (pPaper >= 0.60) {
+            return 'paper';
+        } else if (pScissors >= 0.60) {
+            return 'scissors';
         }
     }
+    return false;
+}
 
-    if (player1Choice == "scissors") {    
-        if (player2Choice == "rock") {
-            return 2;
-        }
-        else {
-            return 1;
+/*
+ * Based on the percentage of the player's plays, chooses computer's next play
+ */
+function choosePlay(playerTendence) {
+    var aWeapons = Object.keys(weapons);
+    //Finds the weapon that beats the player tendence
+    for (i in aWeapons) {
+        if (weapons[aWeapons[i]].beats.indexOf(playerTendence) >= 0) {
+            return aWeapons[i];         
         }
     }
 }
 
+/*
+ * Compare weapons to check the winner
+ */
+function compareWeapons(player1Choice, player2Choice) {
+    //Check if it's a tie
+    if (player1Choice == player2Choice) {
+        return 0;
+    }
+    //Check if player1's weapon beats player2's weapon
+    if (weapons[player1Choice].beats.indexOf(player2Choice) >= 0) {
+    	return 1;         
+    }
+    return 2;
+}
+
+/*
+ * Update scores
+ */
 function updateScores() {
 	player1ScoreBox.innerHTML = player1Score;
 	player2ScoreBox.innerHTML = player2Score;
 }	
 
+/*
+ * Set scores to zero and update them on UI
+ */
 function resetGame() {
     player1Score = 0;
     player2Score = 0;
+    playerRocks = 0;
+    playerPapers = 0;
+    playerScissors = 0;
     updateScores();
 }
 
 //GAME MENU
+
 /* Game modes: 
     1 - Player vs. Computer
     2 - Computer vs. Computer
@@ -105,6 +170,9 @@ var gameMode = 1;
 */
 var gameSymbols = 1;
 
+/*
+ * Switch to Player vs Computer or Computer vs Computer
+ */
 function switchGameMode() {
     gameMode = gameMode == 1 ? 2 : 1;
     resetGame();
@@ -151,6 +219,9 @@ function checkGameMode() {
     }
 }
 
+/*
+ * Switch to hands symbols mode or objects symbols mode
+ */
 function switchGameSymbols() {
     gameSymbols = gameSymbols == 1 ? 2 : 1;
     return checkGameSymbols();
