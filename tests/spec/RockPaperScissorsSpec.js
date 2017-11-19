65994a3e576059e6d0869a0e1aaef2d2095dd401
Igor Bezerra <igor.bezerra96@gmail.com>
2017-11-19 14:41:15 -0300

    Updating code to works with the Revealing module pattern

--- tests/spec/RockPaperScissorsSpec.js
+++ tests/spec/RockPaperScissorsSpec.js
@@ -6,73 +6,64 @@ describe("RockPaperScissors", function() {
 		it("should know that rock beats scissors", function() {
 			player1Choice = 'rock';
 			player2Choice = 'scissors';
-			expect(compareWeapons(player1Choice, player2Choice)).toEqual(1);
+			expect(rockPaperScissors.compareWeapons(player1Choice, player2Choice)).toEqual(1);
 		});
 		
 		it("should know that paper beats rock", function() {
 			player1Choice = 'rock';
 			player2Choice = 'paper';
-			expect(compareWeapons(player1Choice, player2Choice)).toEqual(2);
+			expect(rockPaperScissors.compareWeapons(player1Choice, player2Choice)).toEqual(2);
 		});
 		
 		it("should know that scissors beats paper", function() {
 			player1Choice = 'paper';
 			player2Choice = 'scissors';
-			expect(compareWeapons(player1Choice, player2Choice)).toEqual(2);
+			expect(rockPaperScissors.compareWeapons(player1Choice, player2Choice)).toEqual(2);
 		});
 
 		it("should know that equal weapons result in a tie", function() {
 			player1Choice = 'paper';
 			player2Choice = 'paper';
-			expect(compareWeapons(player1Choice, player2Choice)).toEqual(0);
+			expect(rockPaperScissors.compareWeapons(player1Choice, player2Choice)).toEqual(0);
 
-			//to test when it fails: expect(compareWeapons(player1Choice, player2Choice)).toEqual(1);
+			//to test when it fails: expect(rockPaperScissors.compareWeapons(player1Choice, player2Choice)).toEqual(1);
 		});
 	});
 
 	describe("after some plays", function() {
 		
-		beforeEach(function() {
-			weapons['rock'].counter = 0;
-	      	weapons['paper'].counter = 0;
-			weapons['scissors'].counter = 0;
-	    });
+		// beforeEach(function() {
+		// 	rockPaperScissors.resetGame();
+	 //    });
 
-		it("should know when the player have a tendency to play a specific weapon and say which one it is", function() {
-			//In this case, rock has been played approximately in 66% of the plays
-			totalPlays = 3;
-			weapons['paper'].counter++;
-			weapons['rock'].counter++;
-			weapons['rock'].counter++;
-			expect(checkPlayerTendencies()).toEqual('rock');
-		});
-
-		it("should know when the player doesn't have a tendency to play a specific weapon", function() {
-			//In this case, scissors has been played approximately in 50% of the plays, so, it is not considered a tendency of play
-			totalPlays = 6;
-			weapons['paper'].counter++;
-			weapons['rock'].counter++;
-			weapons['scissors'].counter++;
-			weapons['rock'].counter++;
-			weapons['scissors'].counter++;
-			weapons['scissors'].counter++;
-			expect(checkPlayerTendencies()).toEqual(false);
-		});
-
-		it("should know when the player have a tendency to play a specific weapon after a few plays", function() {
-			//In this case, paper has been played approximately in 60% of the plays
-			totalPlays = 10;
-			weapons['scissors'].counter++;
-			weapons['paper'].counter++;
-			weapons['rock'].counter++;
-			weapons['paper'].counter++;
-			weapons['rock'].counter++;
-			weapons['paper'].counter++;
-			weapons['scissors'].counter++;
-			weapons['paper'].counter++;
-			weapons['paper'].counter++;
-			weapons['paper'].counter++;
-			expect(checkPlayerTendencies()).toEqual('paper');
+		it("should know if the player has a tendency to play a specific weapon and say which one it is", function() {
+			//In this point, rock has been played approximately in 66% of the plays
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('rock');
+			rockPaperScissors.play('rock');
+			expect(rockPaperScissors.checkPlayerTendencies()).toEqual('rock');
+		
+			//In this point, paper has been played approximately in 55% of the plays, so, it is not considered a tendency of play
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('rock');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('rock');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('paper');
+			expect(rockPaperScissors.checkPlayerTendencies()).toEqual(false);
+			
+			//In this point, paper has been played approximately in 63% of the plays
+			rockPaperScissors.play('scissors');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('rock');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('scissors');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('paper');
+			rockPaperScissors.play('paper');
+			expect(rockPaperScissors.checkPlayerTendencies()).toEqual('paper');
 		});
 	});
 });
